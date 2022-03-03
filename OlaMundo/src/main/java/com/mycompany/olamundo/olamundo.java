8de844a64551669aa85bc73c10d613b2e93abfc8
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-03-03 16:05:01 -0300

    Aula 04 de JAVA

--- OlaMundo/src/main/java/com/mycompany/olamundo/olamundo.java
+++
@@ -1,8 +0,0 @@
-
-public class olamundo {
-
-    
-    public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
-}
-}
---
+++ ResolucaoTela/src/resolucaotela/ResolucaoTela.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package resolucaotela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author ruben
+ */
+public class ResolucaoTela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("A resolução do seu sistema é: " + d.width +" X " + d.height);
+    }
+    
+}
