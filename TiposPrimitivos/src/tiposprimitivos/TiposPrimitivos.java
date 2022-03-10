9b0d4d410185a8e0dfec0f7b5d51175d08e481f7
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-03-10 19:21:16 -0300

    Update TiposPrimitivos.java

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -18,9 +18,9 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Digite o nome do aluno: ");
+        System.out.print("Digite o nome do aluno:");
         String nome = teclado.nextLine();
-        System.out.print("Digite a nota do aluno: ");
+        System.out.print("Digite a nota do aluno:");
         float nota = teclado.nextFloat();
         System.out.printf("A nota de %s é %.1f \n", nome, nota);
     }
