61a367bb5b89501576c8992957e28f00d57acd53
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-04-05 22:42:58 -0300

    aula 07

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author ruben
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+        System.out.println("A média é igual a " + m);*/
+        
+        /*int numero = 8;
+        int valor = 3 + numero++;
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        /*int x = 4;
+        x += 2;
+        System.out.println(x);*/
+        
+        /*float v = 8.9f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);*/
+        
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+    }
+    
+}
