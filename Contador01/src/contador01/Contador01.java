0d84f87fbe5b2cca34ac1e99ae33ea347383e885
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-04-22 08:35:39 -0300

    Aula 11 Curso em Video

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,24 @@
+package contador01;
+
+
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+        while(cc<10){
+            cc++;
+            if (cc == 2 || cc == 3 || cc == 4){
+                continue;
+            }
+            if (cc == 7){
+                break;
+            }
+            System.out.println("Cambalhota " + cc);
+        }
+    }
+    
+}
